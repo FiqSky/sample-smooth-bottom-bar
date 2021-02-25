4bec32cf9d389070d23c7ce8c57090fcfab28dd1
FiqSky <fiqihfm05@gmail.com>
2021-02-26 02:39:12 +0700

    Little change

--- app/src/main/java/com/fiqsky/samplesmoothbottombar/DashboardFragment.kt
+++ app/src/main/java/com/fiqsky/samplesmoothbottombar/DashboardFragment.kt
@@ -13,6 +13,6 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+        return inflater.inflate(R.layout.fragment_dashboard, container, false) // R.layout.fragment_dashboard samakan dengan class Fragment nya
     }
 }
